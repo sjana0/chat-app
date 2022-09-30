1a8fc325aea4fa1728f99dcff07142973eda6ca7
Soumyanil Jana <yablue90@gmail.com>
2022-09-30 17:14:58 +0530

    second commit

---
+++ server.js
@@ -0,0 +1,9 @@
+// server.js
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set('view engine', 'ejs');
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World");
+});
+server.listen(3000);
